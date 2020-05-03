cbb320717efb72fe2c1867b33ef1a0a9a3d85e2e
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2020-05-03 13:31:34 -0400

    [Topology] add Node stub class

--- lib/Topology/Node.cpp
+++ lib/Topology/Node.cpp
@@ -1,1 +1,10 @@
 #include "Node.h"
+
+using namespace trek;
+
+Node::Node(int id) : id(id) { }
+
+int Node::getID() const
+{
+    return id;
+}
--- lib/Topology/Node.h
+++ lib/Topology/Node.h
@@ -1,6 +1,38 @@
 #pragma once
 
+namespace trek {
+/**
+ * Node representation
+ * This class represents a node within the network topology.
+ *
+ * There are two primary functions carried out:
+ *   - Talking on the network
+ *   - Talking to client(s)
+ *
+ * For any particular node, there may be multiple clients that need to send or
+ * receive data. These requests are processed by the node and sent out to the
+ * network for handling.
+ *
+ * Therefore, in order to handle client requests, the node will have to talk to
+ * the other nodes, i.e. sending and receiving packets.
+ */
 class Node
 {
+public:
+    /**
+     * Initialise a new node with a given (unique) id
+     * @param id - the node id
+     */
+    explicit Node(int id);
 
+    /**
+     * Get the current node's id
+     * @return the id
+     */
+    int getID() const;
+
+private:
+    int id;
 };
+
+}
