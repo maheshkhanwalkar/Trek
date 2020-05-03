3618a8afb1ad54736e6f6f13177a6946d444d67b
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2020-05-03 15:10:02 -0400

    [Packet] define high-level packet semantics

--- lib/Packet/Packet.cpp
+++ lib/Packet/Packet.cpp
@@ -1,1 +1,5 @@
 #include "Packet.h"
+
+using namespace trek;
+
+Packet::Packet(int id, uint32_t size) : id(id), size(size) {}
--- lib/Packet/Packet.h
+++ lib/Packet/Packet.h
@@ -1,8 +1,40 @@
 #pragma once
+#include <cstdint>
 
 namespace trek {
 
+/**
+ * Packet
+ *
+ * This superclass represents a packet that can be transmitted
+ * on the network.
+ */
 class Packet {
+
+public:
+    /**
+     * Create a new packet
+     * @param id - packet id
+     * @param size - overall packet size
+     */
+    explicit Packet(int id, uint32_t size);
+
+    /**
+     * Drop the packet
+     *
+     * The underlying network has decided to drop (that is, discard) the packet
+     * so perform any implementation-specific event handling
+     */
+    virtual void drop() = 0;
+
+    /**
+     * Destroy the packet (default behaviour)
+     */
+    virtual ~Packet() = default;
+
+protected:
+    int id;
+    uint32_t size;
 };
 
 }
