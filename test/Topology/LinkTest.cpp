803518c9d1077c8781f72376f23092ccf8eba6f1
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2020-05-06 15:12:04 -0400

    [test] [Topology] add duplex test for CapacityLink

--- test/Topology/LinkTest.cpp
+++ test/Topology/LinkTest.cpp
@@ -102,3 +102,36 @@ TEST(CapacityLinkTest, CapacityCheck)
 
     delete link;
 }
+
+TEST(CapacityLinkTest, DuplexTest)
+{
+    std::shared_ptr<trek::Node> first(make_node(0, false));
+    std::shared_ptr<trek::Node> second(make_node(1, false));
+
+    // Throughput is in bits per second, but packet size is in bytes
+    // Simulation time slice is 1 sec
+    trek::Link* link = new trek::CapacityLink(first, second, true, 1024*8, 1);
+
+    ASSERT_FALSE(link->isBusy(first.get()));
+    ASSERT_FALSE(link->isBusy(second.get()));
+
+    link->initiate(first.get(), std::unique_ptr<trek::Packet>(make_packet(2*1024+1)));
+    ASSERT_TRUE(link->isBusy(first.get()));
+    ASSERT_FALSE(link->isBusy(second.get()));
+
+    link->initiate(second.get(), std::unique_ptr<trek::Packet>(make_packet(2*1024-7)));
+    ASSERT_TRUE(link->isBusy(first.get()));
+    ASSERT_TRUE(link->isBusy(second.get()));
+
+    link->transfer();
+    ASSERT_TRUE(link->isBusy(first.get()));
+    ASSERT_TRUE(link->isBusy(second.get()));
+
+    link->transfer();
+    ASSERT_TRUE(link->isBusy(first.get()));
+    ASSERT_FALSE(link->isBusy(second.get()));
+
+    link->transfer();
+    ASSERT_FALSE(link->isBusy(first.get()));
+    ASSERT_FALSE(link->isBusy(second.get()));
+}
