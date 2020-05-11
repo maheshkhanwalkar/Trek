4046a9733e5279dd4871407f37bc84cb60bc91f4
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2020-05-11 13:46:37 -0400

    [Packet] add detailed doc for NestedPacket

--- lib/Packet/NestedPacket.h
+++ lib/Packet/NestedPacket.h
@@ -3,6 +3,41 @@
 
 namespace trek {
 
+/**
+ * Nested packet
+ *
+ * This class represents a packet which contains another packet
+ * within its payload. This is in contrast with the DataPacket class, which
+ * only has data within its payload.
+ *
+ * For example, the following diagram depicts how the packet nesting is
+ * represented physically (in a real packet) vs in this simulation:
+ *
+ * -------------------------------------------------------------
+ * | Ethernet hdr | IP hdr | TCP hdr | Data || Ethernet footer |
+ * -------------------------------------------------------------
+ *
+ * NestedPacket
+ * [ header = Ethernet hdr ]
+ * [ child =
+ *      NestedPacket
+ *      [ header = IP hdr ]
+ *      [ child =
+ *          DataPacket
+ *          [ header = TCP hdr]
+ *          [ data = Data] ] ]
+ *
+ * The Ethernet footer (i.e. frame check sequence) is not simulated, but
+ * its size can be added to the header for size-accuracy.
+ *
+ * This packet allows for different layers to handle different parts of the
+ * packet, very similar to the OS-level network stack. The packet can be
+ * stripped to reveal its inner packet, which can be passed to the right layer
+ * of the processing stack.
+ *
+ * Hence, the framework can support accurate simulation of various protocols
+ * at different layers of network communication (e.g. link, network, transport)
+ */
 class NestedPacket : public Packet {
 
     /**
